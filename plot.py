bc39d081e746a8499dc3fbb98109196f26daa5a4
Julian Kocher <kocherju@gmail.com>
2017-06-11 16:51:45 -0400

    mline actions more fleshed

--- plot.py
+++ plot.py
@@ -4,6 +4,7 @@ from time import sleep
 
 
 CRCM = 2 * 3.14159 * 3.25
+BIGNUM = 999999
 mov_table = []
 obs_table = []
 start = (0,0)
@@ -14,10 +15,7 @@ def start_plot(start, goal):
     plt.plot(start)
     mov_table.append(((0,0),90))
 
-def test_plots():
-    plt.plot(1,1)
-
-#check plot for being previously visited
+#check plot for being previously visited, 0 means you've been there before
 def check_plot(plot):
     cx,cy = plot
     print plot
@@ -79,9 +77,73 @@ def insert_obs_plot(plot):
     obs_table.append(plot)
 
 #return last known position
-def current_position(mov_table):
+def current_position():
     return mov_table[-1]
 
+#use this after inserting the newest plot and adding to mov_table, returns distance(enc) to mline
+def intersect_mline():
+    plot,orientation = current_position()
+    x,__ = plot
+    if x >= 0 and 90 < orientation <= 270:
+        if 90 < orientation <= 180:
+            temp = 180 - orientation
+        elif 180 < orientation <= 270:
+            temp = 270 - orientation
+    elif x < 0 and (0 <= orientation <= 90 or 270 < orientation <= 360):
+        if 0 <= orientation <= 90:
+            temp = orientation
+        elif 270 < orientation <= 360:
+            temp = 360 - orientation
+    else:   #not oriented towards mline
+        return BIGNUM
+
+    dist = math.abs(x) / math.cos(math.radians(temp)) * 100 #convert m to cm
+
+    #if at the mline
+    if dist < 10:
+        n = 0
+        if 0 <= orientation <= 180:
+            if orientation < 90:
+                temp = 180 - orientation
+        elif 180 < orientation <= 360:
+            if orientation is <= 270:
+                turn('right',orientation+90)
+                temp = 90
+                n = 1
+            else:
+                turn('left',(360-orientation)+90)
+                temp = 90
+                n = 2
+        servo(temp)
+        time.sleep(0.5)
+        distance = dream_team_us_dist()
+        goal_dist = distance_to_goal()
+        #if you can see the goal unobstructed, go for it
+        if distance >= goal_dist:
+            enc = (goal_dist/CRCM) * 18.
+            if 0 <= orientation <= 90:
+                turn('left', 90-orientation)
+            elif 90 < orientation <= 180:
+                turn('right', orientation-90)
+            enc_tgt(1,1, int(math.ceil(enc)))
+            fwd()
+            return 0
+        else:   #return to previous orientation
+            if n == 1:
+                turn('left', orientation+90)
+            elif n == 2:
+                turn('right', (360-orientation)+90)
+
+    #number of encoders to the closest mline point on current orientation
+    return (dist/CRCM)*18.
+
+#return distance in cm
+def distance_to_goal():
+    pos,orient = mov_table[-1]
+    gx,gy = goal
+    x,y = pos
+    dist = math.sqrt((gx-x)*(gx-x) + (gy-y)*(gy-y))
+    return dist*100 #convert m to cm
 
 #print all plots
 def print_plots():
@@ -93,7 +155,7 @@ insert_mov_plot((0,2), 180)
 insert_mov_plot((-1, 2), 90)
 insert_mov_plot((-1, 3), 0)
 insert_mov_plot((0,2), 90)
-pos,orient = current_position(mov_table)
+pos,orient = current_position()
 #print "current position: {} {}".format(pos, orient)
 #print_plots()
 #plt.show()
